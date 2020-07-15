0666ef5fac42090faf0cece27f71d34fab86e427
JosefinE7 <fin.jos@hotmail.com>
2020-07-15 18:02:46 +0000

    Added error message for too many github requests

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -68,24 +68,28 @@ function fetchGitHubInformation(event) {
         </div>`);
 
   $.when(
-      $.getJSON(`https://api.github.com/users/${username}`),
-      $.getJSON(`https://api.github.com/users/${username}/repos`)
+        $.getJSON(`https://api.github.com/users/${username}`),
+        $.getJSON(`https://api.github.com/users/${username}/repos`)
     ).then(
-    function (firstResponse, secondResponse) {
-      var userData = firstResponse[0];
-      var repoData = secondResponse[0];
-      $("#gh-user-data").html(userInformationHTML(userData));
-      $("#gh-repo-data").html(repoInformationHTML(repoData));
-    },
-    function (errorResponse) {
-      if (errorResponse.status === 404) {
-        $("#gh-user-data").html(`<h2>No info found for user ${username}</h2>`);
-      } else {
-        console.log(errorResponse);
-        $("#gh-user-data").html(
-          `<h2>Error: ${errorResponse.responseJSON.message}</h2>`);
-      }
-    });
+        function(firstResponse, secondResponse) {
+            var userData = firstResponse[0];
+            var repoData = secondResponse[0];
+            $("#gh-user-data").html(userInformationHTML(userData));
+            $("#gh-repo-data").html(repoInformationHTML(repoData));
+        },
+        function(errorResponse) {
+            if (errorResponse.status === 404) {
+                $("#gh-user-data").html(
+                    `<h2>No info found for user ${username}</h2>`);
+            } else if (errorResponse.status === 403) {
+                var resetTime = new Date(errorResponse.getResponseHeader('X-RateLimit-Reset') * 1000);
+                $("#gh-user-data").html(`<h4>Too many requests, please wait until ${resetTime.toLocaleTimeString()}</h4>`);
+            } else {
+                console.log(errorResponse);
+                $("#gh-user-data").html(
+                    `<h2>Error: ${errorResponse.responseJSON.message}</h2>`);
+            }
+        });
 }
 
 $(document).ready(fetchGitHubInformation);
